1f64489eb6c18a2eabe8cb3196f4a0b39b3a183a
Brett Schules <brettschules@gmail.com>
2018-05-06 10:30:30 -0400

    set interval

--- run.js
+++ run.js
@@ -1,22 +1,67 @@
 class Game {
   constructor() {
+    this.greenElement = document.getElementById('green')
+    this.redElement = document.getElementById('red')
+    this.blueElement = document.getElementById('blue')
+    this.orangeElement = document.getElementById('orange')
     this.addEvents();
+    this.count = 2;
+    this.elementsPicked = [];
+    this.generateRandomSequenceAndGamePlay.call(this)
+
   }
 
   addEvents() {
-    var greenElement = document.getElementById('green')
-    var redElement = document.getElementById('red')
-    var blueElement = document.getElementById('blue')
-    var orangeElement = document.getElementById('orange')
-
-    greenElement.addEventListener('click', this.clicked)
-    redElement.addEventListener('click', this.clicked)
-    blueElement.addEventListener('click', this.clicked)
-    orangeElement.addEventListener('click', this.clicked)
+    this.greenElement.addEventListener('click', this.clicked)
+    this.redElement.addEventListener('click', this.clicked)
+    this.blueElement.addEventListener('click', this.clicked)
+    this.orangeElement.addEventListener('click', this.clicked)
   }
 
-  generateRandomSequence() {
-    var currentRandNum = Math.floor(Math.random() * 4);
+
+  generateRandomSequenceAndGamePlay() {
+    var i = 0;
+      while (i < this.count) {
+        (function(i) {
+        setTimeout(function() {
+          var currentRandNum = Math.floor(Math.random() * 4);
+          const colors = [this.greenElement, this.redElement, this.blueElement, this.orangeElement];
+          debugger
+          var currentElement = colors[currentRandNum]
+          console.log(colors, currentRandNum)
+        }, 1000 * i)
+      })(i++)
+    }
+    console.log([this.greenElement, this.redElement, this.blueElement, this.orangeElement]);
+
+    this.checkIfPlaySelectedCorrect()
+
+  }
+
+    // for (var i = 0; i < this.count; i++) {
+    //   // setTimeout(function(){
+    //   //   // var currentElement = colors[currentRandNum];
+    //   //   // this.displayColor(currentElement)
+    //   //   // this.elementsPicked.push(currentElement)
+    //   //   console.log('test')
+    //   // },  2 * 1000);
+    //
+    //   var delay = 1000;
+    //   setTimeout(function () {
+    //     console.log('test')
+    // }, delay);
+    // delay += 5000;
+
+    // }
+
+
+  displayColor(currentElement) {
+    console.log(currentElement)
+    currentElement.style.opacity = '0.5'
+  }
+
+  checkIfPlaySelectedCorrect() {
+
   }
 
   clicked() {
@@ -28,7 +73,7 @@ class Game {
 
 class App {
   constructor() {
-    new Game()
+    var g = new Game()
   }
 }
 

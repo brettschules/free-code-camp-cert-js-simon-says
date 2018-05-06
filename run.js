4a2988e57eab8dc917733092722f1e99de0e3880
Brett Schules <brettschules@gmail.com>
2018-05-06 16:06:28 -0400

    Alomst...

--- run.js
+++ run.js
@@ -4,27 +4,31 @@ class Game {
     this.redElement = document.getElementById('red')
     this.blueElement = document.getElementById('blue')
     this.orangeElement = document.getElementById('orange')
-    this.addEvents();
-    this.count = 2;
+    this.count = 1;
+    this.index = 0;
     this.elementsPicked = [];
     this.generateRandomSequenceAndGamePlay.call(this)
 
   }
 
   addEvents() {
-    this.greenElement.addEventListener('click', this.clicked)
-    this.redElement.addEventListener('click', this.clicked)
-    this.blueElement.addEventListener('click', this.clicked)
-    this.orangeElement.addEventListener('click', this.clicked)
+    this.greenElement.addEventListener('click', this.greenClicked.bind(this))
+    this.redElement.addEventListener('click', this.redClicked.bind(this))
+    this.blueElement.addEventListener('click', this.blueClicked.bind(this))
+    this.orangeElement.addEventListener('click', this.orangeClicked.bind(this))
   }
 
 
   generateRandomSequenceAndGamePlay() {
     var displayColor = this.displayColor
+    var checkIfPlaySelectedCorrect = this.checkIfPlaySelectedCorrect
     var green = this.greenElement
     var red = this.redElement
     var blue = this.blueElement
     var orange = this.orangeElement
+    var elementsPickedd = []
+    var count = this.count
+    var ctrl = this
     var i = 0;
       while (i < this.count) {
         (function(i) {
@@ -32,31 +36,98 @@ class Game {
           var currentRandNum = Math.floor(Math.random() * 4);
           const colors = [green, red, blue, orange];
           var currentElement = colors[currentRandNum]
-          displayColor(currentElement)
-          console.log(colors, currentRandNum)
+          elementsPickedd.push(currentElement)
+          displayColor(currentElement, elementsPickedd)
+          if (i === count - 1) {
+            checkIfPlaySelectedCorrect(elementsPickedd, ctrl)
+          }
         }, 1000 * i)
       })(i++)
     }
-
-    this.checkIfPlaySelectedCorrect()
-
   }
 
-  displayColor(currentElement) {
-    console.log(currentElement)
+  displayColor(currentElement, elementsPickedd) {
     currentElement.style.opacity = '0.5'
     setTimeout(function(){
       currentElement.style.opacity = '1.0'
       },  1000);
   }
 
-  checkIfPlaySelectedCorrect() {
+  checkIfPlaySelectedCorrect(elementsPickedd, ctrl) {
+    console.log(elementsPickedd, 'fd')
+    ctrl.elementsPicked = elementsPickedd
+    var i = 0
+    if (true) {
+
+    }
+    ctrl.count = 2
+    // ctrl.generateRandomSequenceAndGamePlay()
+  }
+  greenClicked() {
+    var colorClicked = this;
+    console.log(colorClicked)
+    if (this.elementsPicked.length !== 0) {
+      if (this.greenElement === this.elementsPicked[this.index]) {
+        this.index++
+        debugger
+      }
+    }
+
+    if (this.elementsPicked.length - 1 === this.index) {
+      this.count++
+      this.generateRandomSequenceAndGamePlay()
+    }
 
   }
 
-  clicked() {
-    var colorClicked = this.id;
+  redClicked() {
+    var colorClicked = this;
+    if (this.elementsPicked.length !== 0) {
+      if (this.redElement === this.elementsPicked[this.index]) {
+        this.index++
+      }
+    }
+
+    if (this.elementsPicked.length - 1 === this.index) {
+      this.count++
+      this.generateRandomSequenceAndGamePlay()
+    }
+
+  }
+
+  blueClicked() {
+    var colorClicked = this;
     console.log(colorClicked)
+    if (this.elementsPicked.length !== 0) {
+      if (this.blueElement === this.elementsPicked[this.index]) {
+        this.index++
+      }
+    }
+
+    if (this.elementsPicked.length - 1 === this.index) {
+      this.count++
+      this.generateRandomSequenceAndGamePlay()
+    }
+
+  }
+
+  orangeClicked() {
+    var colorClicked = this;
+    console.log(colorClicked)
+    if (this.elementsPicked.length !== 0) {
+      if (this.orangeElement === this.elementsPicked[this.index]) {
+        this.index++
+      }
+    }
+
+    if (this.elementsPicked.length - 1 === this.index) {
+      this.count++
+      this.generateRandomSequenceAndGamePlay()
+    }
+
+  }
+
+  restart() {
 
   }
 }
@@ -64,6 +135,8 @@ class Game {
 class App {
   constructor() {
     var g = new Game()
+    g.addEvents()
+
   }
 }
 
